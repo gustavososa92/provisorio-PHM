a3ad96d401d8c86b337ccbd982fd3d2e6f12ac74
Gustavo Sosa <gustavososa92@gmail.com>
2021-03-14 18:45:42 -0300

    commit de front2

---
+++ front/pregunta3-front/src/componentes/footer/footer.jsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <i class="fab fa-facebook-square"></i>
+        <span> Pregunta3 - PHM\ 2021</span>
+        <span>. Grupo 7 - All rights reserved</span>
+      </footer>
+    </div>
+  )
+}
