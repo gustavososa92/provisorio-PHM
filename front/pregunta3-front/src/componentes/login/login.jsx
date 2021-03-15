27617d4bae021903f924e4fedfac72ced667968e
Nahuel <cristiannahuel95@gmail.com>
2021-03-15 00:57:39 -0300

    Login usa useState para ambos inputs

--- front/pregunta3-front/src/componentes/login/login.jsx
+++ front/pregunta3-front/src/componentes/login/login.jsx
@@ -1,22 +1,28 @@
+import { useState } from "react"
 import { Form, FormGroup, Button } from "react-bootstrap"
 import { Footer } from "../footer/footer"
 import "./login.css"
 
 export const Login = () => {
+  const [usuario, setUsuario] = useState("")
+  const [pass, setPass] = useState("")
+
   return (
     <div className="login-container">
       <Form className="login">
         <h2 style={{ textAlign: "center", marginBottom: "2rem" }}>Pregunta3!</h2>
         <FormGroup>
           <Form.Label>Usuario</Form.Label>
-          <Form.Control size="lg" placeholder="Ingrese usuario..." />
+          <Form.Control size="lg" placeholder="Ingrese usuario..." value={usuario} onChange={(e) => setUsuario(e.target.value)} />
         </FormGroup>
         <FormGroup>
           <Form.Label>Contraseña</Form.Label>
-          <Form.Control type="password" size="lg" placeholder="Ingrese contraseña..." />
+          <Form.Control type="password" size="lg" placeholder="Ingrese contraseña..." value={pass} onChange={(e) => setPass(e.target.value)} />
         </FormGroup>
         <div style={{ textAlign: "center", marginTop: "2rem" }}>
-          <Button variant="info">Ingresar</Button>
+          <Button variant="info" onClick={() => console.log(usuario, pass)}>
+            Ingresar
+          </Button>
         </div>
       </Form>
     </div>
