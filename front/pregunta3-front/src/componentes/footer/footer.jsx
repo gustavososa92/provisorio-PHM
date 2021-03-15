f615ca50d1bec4103ef8503cc66f557322e3bbae
Nahuel <cristiannahuel95@gmail.com>
2021-03-15 12:31:20 -0300

    footer usa su propia hoja de estilos + pequeños cambios de vista en el mismo

--- front/pregunta3-front/src/componentes/footer/footer.jsx
+++ front/pregunta3-front/src/componentes/footer/footer.jsx
@@ -1,11 +1,11 @@
+import "./footer.css"
+
 export const Footer = () => {
   return (
-    <div>
-      <footer>
-        <i class="fab fa-facebook-square"></i>
-        <span> Pregunta3 - PHM\ 2021</span>
-        <span>. Grupo 7 - All rights reserved</span>
-      </footer>
-    </div>
+    <footer className="footer">
+      <i class="fab fa-facebook-square"></i>
+      <span> Pregunta3 - PHM\ 2021</span>
+      <span>. Grupo 7 - All rights reserved</span>
+    </footer>
   )
 }
